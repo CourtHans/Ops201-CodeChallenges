64695382d36b897fbafa39e163253c27bd4f0ba5
CourtHans <cornta@yahoo.com>
2023-02-16 19:42:56 -0800

    this is my commit message

---
+++ practice.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author:
+# Date of latest revision:
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+echo "this is working perfectly"
+
+
+# End
